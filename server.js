4bdfae1717d4c4f5edbaec2fab9967e05a4a4031
SOM-0x3B1 <81362073+SOM-0x3B1@users.noreply.github.com>
2024-04-23 22:17:52 +0200

    Clean up code

--- server.js
+++ server.js
@@ -17,29 +17,12 @@ app.disable('x-powered-by')
 
 app.set('trust proxy', 1);
 app.use(urlencoded({ extended: true, limit: '3mb' }));
-app.use(express.static('/'));
+app.use(express.static('public'))
 
 
 app.get('/', (_, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-    //visits++;
+    res.sendFile(join(__dirname, 'public/index.html'));
 });
-
-app.get('*', (req, res) => {
-    const hasExtension = req.path.includes('.');
-    const joinedPath = join(__dirname, '/', (hasExtension ? req.path : (req.path + '.html')));
-
-    console.log(joinedPath)
-
-    if (fs.existsSync(joinedPath))
-        res.sendFile(joinedPath);
-    else {
-        res.writeHead(307, { 'Location': 'https://www.onekilobit.eu/404' });
-        res.end();
-    }
-});
-
-
 
 const server = http.createServer(app)
 server.listen(80, () => { });
@@ -50,13 +33,12 @@ const port = new SerialPort({
     baudRate: 9600,
 });
 
-
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n\r' }))
 
 
 let gPitch, gRoll;
-parser.on('data', function (data) {   
-    const line = data.toString().split(' ');    
+parser.on('data', function (data) {
+    const line = data.toString().split(' ');
     gPitch = parseInt(line[0]);
     gRoll = parseInt(line[1]);
 });
@@ -69,7 +51,7 @@ io.on('connection', (socket) => {
         socket.emit('sendData', { pitch: gPitch, roll: gRoll });
     }, 50);
 
-    socket.on('disconnect', () => {        
+    socket.on('disconnect', () => {
         clearInterval(x);
     })
 });
